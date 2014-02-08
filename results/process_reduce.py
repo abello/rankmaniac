7b777bc5b10bfbc191f8d911cab34983d7c92fb5
Peter Henry <phenryextra@zoho.com>
2014-02-07 23:16:05 -0800

    it does SOMETHING when run on Amazon

--- results/process_reduce.py
+++ results/process_reduce.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import os.path
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-from rankmaniac import Rankmaniac
 
 #
 # This program simply represents the identity function.
@@ -12,7 +8,6 @@ from rankmaniac import Rankmaniac
 
 for i in range(20):
 	sys.stdout.write('FinalRank:1\t' + str(i) + '\n')
-
 
 #for line in sys.stdin:   
 #    sys.stdout.write(line)
