e06645be4a69018e1cba7a8ec8a8981819f96eaa
Victor Duan <victor.w.duan@gmail.com>
2014-02-12 09:31:12 -0800

    got pagerank functions working

--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -9,23 +9,20 @@ ALPHA = 0.85
 
 # read a line of input
 for line in sys.stdin:
-    index = line.find('\t')
-    line = line[index+1:]
+    line = line.split()[1]
  
     # if line starts with '_' it's adj info; pass it along as is
     if line[0] == '_':
-        sys.stdout.write("adj\t" + line) # (doesn't need newline)
+        sys.stdout.write("adj\t" + line + '\n')
 
     # else line starts with '+' and it's contrib info; grab it
     elif line[0] == '+':
-        # decode (unescape) and un-pickle the line
-        line = line.decode('string-escape')
-        info = pickle.loads(line[1:])
+        info = line[1:].split(',')
 
         # save each value the line holds
-        iteration = info[0]
-        node      = info[1]
-        contrib   = info[2]
+        iteration = int(info[0])
+        node      = int(info[1])
+        contrib   = float(info[2])
 
         if node in result.keys():
             # increment node's pagerank in {result} with another contribution
@@ -35,11 +32,13 @@ for line in sys.stdin:
             result[node] = contrib
     else:
         #victor
+        sys.stderr.write("elsecase pagerank_reduce\n")
         pass
 
 # loop over every node with pagerank and emit it
 for node in result.keys():
     # (node, rank) pair lines start with a '+'
-    out = '+' + pickle.dumps((iteration, node, ALPHA * result[node]  + (1 - ALPHA)))
-    out = str(node) + "\t" + out.encode('string-escape')
+    rank = ALPHA * result[node] + (1 - ALPHA)
+    out = str(node) + '\t+' + str(iteration) + ',' + str(node) + ',' + str(rank)
     print out # (needs newline)
+
